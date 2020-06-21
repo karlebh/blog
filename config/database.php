3e9bf68344305e9482eae3f76f9e5f2b002c83a6
caleb <akejucaleb@yahoo.com>
2020-06-21 02:10:12 +0100

    final commit

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'blog'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'http://blog.com',
+            'host' => 'blog.com',
+            'port' => '3306',
+            'database' => 'blog',
+            'username' => 'caleb',
+            'password' => '6MsxoTA59hRUg5e0jtvm3i5HRB5hRtrxRqG0sr9IvlB0dYSFcP32iitWQ2Wh',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
